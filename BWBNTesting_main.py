a38ecb7a87f81acf96d71d0cbdc02561f458ea25
Jaehwan Jeon <dudesup123@snu.ac.kr>
2023-10-16 22:02:30 +0900

    Stable - normalization only for h

--- BWBNTesting_main.py
+++ BWBNTesting_main.py
@@ -21,10 +21,10 @@ import ResultAnalysis
 
 generate_EQ = False
 preprocess = False
-train = False
+train = True
 analyze_result = True
 
-Title = 'BWBN_energy_input_Drucker'
+Title = 'BWBN_energy_h_only_normalization'
 
 
 EQ_data_dir = '/home/jaehwan/Python Project/DLCM/Data'
