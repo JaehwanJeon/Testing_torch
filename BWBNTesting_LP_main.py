8ebe4a80ff76f1b0a0805d967916e7e2635f0e4a
Jaehwan Jeon <dudesup123@snu.ac.kr>
2023-12-11 13:22:41 +0900

    d17aa25b - Fixing bug (not fully explored)

--- BWBNTesting_LP_main.py
+++ BWBNTesting_LP_main.py
@@ -20,8 +20,8 @@ import Training
 import ResultAnalysis
 
 generate_EQ = False
-preprocess = True
-train = True
+preprocess = False
+train = False
 analyze_result = True
 
 Title = 'BWBN_h_energy_diff_disp'
@@ -130,8 +130,8 @@ X_val, X_test, y_val, y_test = Data['X_val'], Data['X_test'], Data['y_val'], Dat
 del Data
 X_val[:, :, 1] = np.diff(X_val[:, :, 0], axis=1, prepend=0)
 X_val, mask_val, X_test, mask_test = X_val[:, :, :2], X_val[:, :, 2], X_test[:, :, :2], X_test[:, :, 2]
-model_paths = [os.path.normpath(os.path.join(model_dir, './' + Title + '_linear_protocol' + '_checkpoint_{}.pth'.format(i+checkpoint_epoch))) for i in range(0, num_epochs, checkpoint_epoch)]
-# model_paths = [os.path.normpath(os.path.join(model_dir, './' + Title + '_linear_protocol' + '_checkpoint_925.pth'))]
+model_paths = [os.path.normpath(os.path.join(model_dir, './' + LPTitle + '_checkpoint_{}.pth'.format(i+checkpoint_epoch))) for i in range(0, num_epochs, checkpoint_epoch)]
+# model_paths = [os.path.normpath(os.path.join(model_dir, './' + LPTitle + '_checkpoint_925.pth'))]
 result_plot_dir = '/home/jaehwan/Python Project/DLCM/Testing_torch/Result Plots'
 os.makedirs(result_plot_dir, exist_ok=True)
 
