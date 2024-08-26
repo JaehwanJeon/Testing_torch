f41be20af08b80df4881602213757da1a1668fec
Jaehwan Jeon <dudesup123@snu.ac.kr>
2024-08-26 09:32:58 +0900

    Xu_OP_4_main.py run
    Checkpoint Xu_OP_4_main.py
    
    Xu_OP_4_main.py 돌리기

--- Training.py
+++ Training.py
@@ -24,6 +24,7 @@ def train(X_train,
           existing_checkpoint=False,
           augmentation_rate=False,
           result_plot_dir=False,
+          batch_size=False,
           device = torch.device('cuda:0')):
 
     if not result_plot_dir:
@@ -45,53 +46,100 @@ def train(X_train,
     states = None
     model.eval()
     outputs, energies, states = model(add_diff(X_train), states)
-    LossClass = Loss(outputs[:, :, 0], y_train, energies[:, :, 0], mask_train, device)
+    if batch_size != False:
+        batch_idx = torch.randperm(X_train.shape[0])[:batch_size]
+        LossClass = Loss(outputs[batch_idx, :, 0], y_train[batch_idx], energies[batch_idx, :, 0], mask_train[batch_idx], device)
+    else:
+        LossClass = Loss(outputs[:, :, 0], y_train, energies[:, :, 0], mask_train, device)
     criterion = LossClass.combined_loss
 
     # Training loop
     model.train()
     epoch_losses = []
     validation_df = pd.DataFrame(columns=['Epoch', 'Loss', 'MSE', 'PhysLoss'])
-    for epoch in range(num_epochs):
-        if augmentation_rate != False:
-            augmented_idx = np.random.choice(X_train_.shape[1], int(X_train_.shape[1] * augmentation_rate), replace=False)
-            augmented_idx = np.sort(augmented_idx)
-            X_train, y_train, mask_train = X_train_[:, augmented_idx], y_train_[:, augmented_idx], mask_train_[:, augmented_idx]
-            X_train = add_diff(X_train)
-        else:
-            X_train = add_diff(X_train_)
-        states = None
-        losses = []
-        print('Epoch:', epoch+1)
-        for step in range(0, X_train.shape[1]-window_size, window_size):
-            # print('Step:', step, '//', X_train.shape[1]-window_size, end='\r')
-            inputs = X_train[:, step:step+window_size]
-            labels = y_train[:, step:step+window_size]
+    if batch_size != False:
+        for epoch in range(num_epochs):
+            losses = []
+            print('Epoch:', epoch+1)
+            for STEP in range(X_train_.shape[0]//batch_size + 1):
+                if augmentation_rate != False:
+                    augmented_idx = torch.randperm(X_train_.shape[1])[:int(X_train_.shape[1] * augmentation_rate)].to(device)
+                    augmented_idx, _ = torch.sort(augmented_idx)
+                    X_train, y_train, mask_train = X_train_[:, augmented_idx], y_train_[:, augmented_idx], mask_train_[:, augmented_idx]
+                    X_train = add_diff(X_train)
+                batch_idx = torch.randperm(X_train.shape[1])[:batch_size]
+                states = None
+                for step in range(0, X_train.shape[1]-int(window_size * augmentation_rate) + 1, int(window_size*augmentation_rate)):
+                    # print('Step:', step, '//', X_train.shape[1]-window_size, end='\r')
+                    inputs = X_train[batch_idx, step:step+window_size]
+                    labels = y_train[batch_idx, step:step+window_size]
 
-            outputs, energies,  states = model(inputs, states)
+                    outputs, energies,  states = model(inputs, states)
 
-            loss = criterion(outputs[:, :, 0], labels, energies[:, :, 0], mask_train[:, step:step+window_size], alpha=alpha)
-            losses.append(loss.item())
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+                    loss = criterion(outputs[:, :, 0], labels, energies[:, :, 0], mask_train[batch_idx, step:step+window_size], alpha=alpha)
+                    losses.append(loss.item())
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
 
-            states = tuple(state.detach() for state in states)
+                    states = tuple(state.detach() for state in states)
+                
+            avg_loss = sum(losses) / len(losses)
+            epoch_losses.append(avg_loss)
+            print('\n', f'Epoch {epoch + 1}/{num_epochs}, Loss: {avg_loss}')        
+
+            # 일정 주기마다 손실 출력
+            if (epoch + 1) % checkpoint_epoch == 0:
+                outputs, energies,  _ = model(X_val)
+                loss = criterion(outputs[:, :, 0], y_val, energies[:, :, 0], mask_val, alpha=alpha)
+                new_row = {'Epoch': epoch+1, 'Loss': loss.item(), 'MSE': LossClass.mse_loss.item(), 'PhysLoss': LossClass.phys_loss.item(), 'TrainLoss': avg_loss}
+                validation_df = validation_df.append(new_row, ignore_index=True)
+                validation_df.to_csv(os.path.normpath(os.path.join(result_plot_dir, title + '_loss.csv')), index=False)
+                val_loss.append(loss.item())
+                print(f'Validation Loss: {loss.item()}')
+                torch.save(model.state_dict(), os.path.normpath(os.path.join(checkpoint_dir, title + '_checkpoint_{}.pth'.format(epoch+1))))
+
+    else:
+        for epoch in range(num_epochs):
+            if augmentation_rate != False:
+                augmented_idx = np.random.choice(X_train_.shape[1], int(X_train_.shape[1] * augmentation_rate), replace=False)
+                augmented_idx = np.sort(augmented_idx)
+                X_train, y_train, mask_train = X_train_[:, augmented_idx], y_train_[:, augmented_idx], mask_train_[:, augmented_idx]
+                X_train = add_diff(X_train)
+            else:
+                X_train = add_diff(X_train_)
+            states = None
+            losses = []
+            print('Epoch:', epoch+1)
+            for step in range(0, X_train.shape[1]-int(window_size * augmentation_rate) + 1, int(window_size*augmentation_rate)):
+                # print('Step:', step, '//', X_train.shape[1]-window_size, end='\r')
+                inputs = X_train[:, step:step+window_size]
+                labels = y_train[:, step:step+window_size]
+
+                outputs, energies,  states = model(inputs, states)
+
+                loss = criterion(outputs[:, :, 0], labels, energies[:, :, 0], mask_train[:, step:step+window_size], alpha=alpha)
+                losses.append(loss.item())
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                states = tuple(state.detach() for state in states)
+                
+            avg_loss = sum(losses) / len(losses)
+            epoch_losses.append(avg_loss)
+            print('\n', f'Epoch {epoch + 1}/{num_epochs}, Loss: {avg_loss}')        
+
+            # 일정 주기마다 손실 출력
+            if (epoch + 1) % checkpoint_epoch == 0:
+                outputs, energies,  _ = model(X_val)
+                loss = criterion(outputs[:, :, 0], y_val, energies[:, :, 0], mask_val, alpha=alpha)
+                new_row = {'Epoch': epoch+1, 'Loss': loss.item(), 'MSE': LossClass.mse_loss.item(), 'PhysLoss': LossClass.phys_loss.item()}
+                validation_df = validation_df.append(new_row, ignore_index=True)
+                validation_df.to_csv(os.path.normpath(os.path.join(result_plot_dir, title + '_loss.csv')), index=False)
+                val_loss.append(loss.item())
+                print(f'Validation Loss: {loss.item()}')
+                torch.save(model.state_dict(), os.path.normpath(os.path.join(checkpoint_dir, title + '_checkpoint_{}.pth'.format(epoch+1))))
             
-        avg_loss = sum(losses) / len(losses)
-        epoch_losses.append(avg_loss)
-        print('\n', f'Epoch {epoch + 1}/{num_epochs}, Loss: {avg_loss}')        
-
-        # 일정 주기마다 손실 출력
-        if (epoch + 1) % checkpoint_epoch == 0:
-            outputs, energies,  _ = model(X_val)
-            loss = criterion(outputs[:, :, 0], y_val, energies[:, :, 0], mask_val, alpha=alpha)
-            new_row = {'Epoch': epoch+1, 'Loss': loss.item(), 'MSE': LossClass.mse_loss.item(), 'PhysLoss': LossClass.phys_loss.item()}
-            validation_df = validation_df.append(new_row, ignore_index=True)
-            validation_df.to_csv(os.path.normpath(os.path.join(result_plot_dir, title + '_loss.csv')), index=False)
-            val_loss.append(loss.item())
-            print(f'Validation Loss: {loss.item()}')
-            torch.save(model.state_dict(), os.path.normpath(os.path.join(checkpoint_dir, title + '_checkpoint_{}.pth'.format(epoch+1))))
-        
     np.savez(os.path.normpath(os.path.join(checkpoint_dir, title + '_losses.npz')), train_loss=epoch_losses, val_loss=val_loss)
     return model
---
+++ Xu_OP_4_main.py
@@ -0,0 +1,109 @@
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+import random
+from backend import *
+import scipy.io
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # multi-GPU 환경에서 모든 GPU에 seed 설정
+    torch.backends.cudnn.deterministic = True  # cuDNN을 determinstic 모드로 설정
+    torch.backends.cudnn.benchmark = False  # cuDNN benchmarking을 off로 설정
+    np.random.seed(seed)
+    random.seed(seed)
+
+set_seed(42)
+import Training
+import ResultAnalysis
+
+train = True
+analyze_result = True
+
+Title = 'OP_4_PE_PI_DA'
+
+
+hysteresis_data_dir = 'Xu_hysteresis'
+
+
+with open('EQ_list.txt', 'r') as f:
+    EQ_list_ = [line.strip() for line in f]
+print(EQ_list_[:3])
+
+EQ_list = []
+for i, EQ in enumerate(EQ_list_):
+    EQ_name = './'+EQ
+    EQ_list.append(os.path.normpath(os.path.join('/home/jaehwan/Python Project/DLCM/Data', EQ_name)).replace("\\", "/"))
+print(EQ_list[:3])
+
+
+seed = 0
+
+hysteresis_path = os.path.normpath(os.path.join(hysteresis_data_dir, './' + 'data_OP_4_final.mat'))
+Data = scipy.io.loadmat(hysteresis_path)
+X_train, y_train = Data['X_train'], Data['y_train']
+X_val, y_val = Data['X_valid'], Data['y_valid']
+X_test, y_test = Data['X_test'], Data['y_test']
+processed_data_dir = hysteresis_data_dir
+# X_train_diff, X_val_diff, X_test_diff = np.diff(X_train, axis=1, prepend=0), np.diff(X_val, axis=1, prepend=0), np.diff(X_test, axis=1, prepend=0)
+# X_train, X_val, X_test = np.concatenate([X_train, X_train_diff], axis=2), np.concatenate([X_val, X_val_diff], axis=2), np.concatenate([X_test, X_test_diff], axis=2)
+y_train, y_val, y_test = y_train[:, :, 0], y_val[:, :, 0], y_test[:, :, 0]
+mask_train, mask_val, mask_test = np.ones_like(y_train), np.ones_like(y_val), np.ones_like(y_test)
+
+# Training
+device = torch.device('cuda:0')
+nn_size = 64
+model = CustomLSTM(2, nn_size, 1)
+alpha = 0.2
+num_epochs = 1000
+model_dir = '/home/jaehwan/Python Project/DLCM/Testing_torch/Models'
+os.makedirs(model_dir, exist_ok=True) 
+window_size = 1000
+checkpoint_epoch = 5
+pretrained = False
+checkpoint = False
+augmentation_rate = 0.95
+if pretrained != False:
+    checkpoint = torch.load(pretrained)
+
+
+result_plot_dir = '/home/jaehwan/Python Project/DLCM/Testing_torch/Result Plots'
+os.makedirs(result_plot_dir, exist_ok=True)
+
+if train:
+    model = Training.train(X_train,
+                                y_train,
+                                mask_train,
+                                X_val,
+                                y_val,
+                                mask_val,
+                                model,
+                                num_epochs,
+                                alpha,
+                                window_size,
+                                checkpoint_dir=model_dir,
+                                title=Title,
+                                checkpoint_epoch=checkpoint_epoch,
+                                existing_checkpoint=checkpoint,
+                                augmentation_rate=augmentation_rate,
+                                result_plot_dir=result_plot_dir,
+                                batch_size=128,
+                                device=device)
+
+model_paths = [os.path.normpath(os.path.join(model_dir, './' + Title + '_checkpoint_{}.pth'.format(i+checkpoint_epoch))) for i in range(0, num_epochs, checkpoint_epoch)]
+
+if analyze_result:
+    ResultAnalysis.result_plot(X_val,
+                y_val,
+                mask_val,
+                X_test,
+                y_test,
+                mask_test,
+                model,
+                model_dir,
+                model_paths,
+                Title,
+                result_plot_dir,
+                device = device)
